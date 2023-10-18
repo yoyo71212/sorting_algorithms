49ef91863675c4e45fe6c072f8709fcfd188a40b
Yusuf Swelam <yusufswelam2@gmail.com>
2023-10-18 11:59:15 -0700

    Implement selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+/**
+ * selection_sort - Function that sorts an array of integers in ascending order
+ * using the Selection sort algorithm
+ *
+ * @array: The array to be sorted
+ * @size: The number of elements in the array
+ *
+ * Return: void
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int min, temp;
+
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+		}
+
+		temp = array[i];
+		array[i] = array[min];
+		array[min] = temp;
+		print_array(array, size);
+	}
+}
