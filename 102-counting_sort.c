7022eeeaa10bea40808b75679d5d8aba534d5449
Yusuf Swelam <yusufswelam2@gmail.com>
2023-10-28 14:33:41 -0700

    Revert "Implement counting sort"
    This reverts commit 5ee7e52bdbc3c4b3baab40aef3f981dd8f1d1584.

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -63,6 +63,8 @@ void counting_sort(int *array, size_t size)
 		counting_array[array[i]]++;
 	}
 
+	print_array(counting_array, max + 1);
+
 	j = 0;
 	for (i = 0; i <= max; i++)
 	{
@@ -74,4 +76,5 @@ void counting_sort(int *array, size_t size)
 	}
 
 	free(counting_array);
+	print_array(array, size);
 }
